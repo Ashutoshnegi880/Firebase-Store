b5c4eb6b6c10368778b126a5fadabeff25eabe29
Ashutoshnegi880 <68495328+Ashutoshnegi880@users.noreply.github.com>
2021-10-04 14:03:12 +0530

    Firebase Store - Adding and Retrieving data from firestore

--- app/src/main/java/com/example/firebasestore/MainActivity.kt
+++ app/src/main/java/com/example/firebasestore/MainActivity.kt
@@ -31,11 +31,9 @@ class MainActivity : AppCompatActivity() {
             savePerson(person)
         }
 
-        subscribeToRealTimeUpdates()
-
-//        btnRetrieveData.setOnClickListener {
-//            retrievePerson()
-//        }
+        btnRetrieveData.setOnClickListener {
+            retrievePerson()
+        }
 
     }
 
@@ -71,23 +69,6 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
             }
 
-        }
-    }
-
-    private fun subscribeToRealTimeUpdates() {
-        personCollectionRef.addSnapshotListener {querySnapshot, firebaseFirestoreException ->
-            firebaseFirestoreException?.let {
-                Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
-                return@addSnapshotListener
-            }
-            querySnapshot?.let {
-                val sb = StringBuilder()
-                for (document in it){
-                    val person = document.toObject<Person>()
-                    sb.append("$person\n")
-                }
-                tvPersons.text = sb.toString()
-            }
         }
     }
 }
